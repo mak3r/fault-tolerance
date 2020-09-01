95df9356c480d4f7073b50124dede45c7c6d323d
Mark Abrams <mabrams@helpguest.com>
2020-09-01 11:27:48 -0400

    fix missing space in conditional

--- bandwidth/entrypoint.sh
+++ bandwidth/entrypoint.sh
@@ -3,7 +3,7 @@
 LOW_LIMIT=$1
 rate=`echo "$(speedtest --no-upload --csv --csv-delimiter '|' | cut -d '|' -f 7)/1000000" | bc`
 
-if [[ "$rate" -gt "$LOW_LIMIT"]]; then
+if [[ "$rate" -gt "$LOW_LIMIT" ]]; then
 	echo "Rate $rate is acceptable."
 	exit 0;
 else
