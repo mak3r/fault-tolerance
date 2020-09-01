bc52be5d31fc4b4689e88ad3a0f84d51236c7beb
Mark Abrams <mabrams@helpguest.com>
2020-09-01 14:39:09 -0400

    fix bugs in the algorithm changes to use decimal places appear to have been untested.

--- bandwidth/entrypoint.sh
+++ bandwidth/entrypoint.sh
@@ -3,7 +3,7 @@
 LOW_LIMIT=$1
 rate=`echo "scale=2; $(speedtest --no-upload --csv --csv-delimiter '|' | cut -d '|' -f 7)/8388608" | bc -l`
 
-if [[ "$rate" -gt "$LOW_LIMIT" ]]; then
+if (( $(echo "$rate > $LOW_LIMIT" | bc -l) )); then
 	echo "Rate $rate MB/s is acceptable."
 	exit 0;
 else
