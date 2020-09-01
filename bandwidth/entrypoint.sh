8575ca3dc0933a67354f51d00d616dc7ebbe0804
Mark Abrams <mabrams@helpguest.com>
2020-09-01 16:07:19 -0400

    setup bandwidth to loop until successful this will be an init container after all.

--- bandwidth/entrypoint.sh
+++ bandwidth/entrypoint.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+LIMITED=1
 LOW_LIMIT=$1
-rate=`echo "scale=2; $(speedtest --no-upload --csv --csv-delimiter '|' | cut -d '|' -f 7)/8388608" | bc -l`
 
-if (( $(echo "$rate > $LOW_LIMIT" | bc -l) )); then
-	echo "Rate $rate MB/s is acceptable."
-	exit 0;
-else
-	echo "Rate $rate MB/s is too low. Threshold is $LOW_LIMIT MB/s"
-	exit `echo "$rate/1" | bc`
-fi
+function check() {
+	echo "scale=2; $(speedtest --no-upload --csv --csv-delimiter '|' | cut -d '|' -f 7)/8388608" | bc -l	
+}
+
+function stop() {
+	LIMITED=0
+}
+
+function run() {
+	rate=check
+	while [[ "$LIMITED" -ne "0" ]]; do
+		if (( $(echo "$rate > $LOW_LIMIT" | bc -l) )); then
+			LIMITED=0
+			echo "Rate $rate MB/s is acceptable."
+		else
+			echo "Rate $rate MB/s is too low. Threshold is $LOW_LIMIT MB/s"
+			sleep 5
+			LIMITED=1
+		fi
+	done
+}
+
+trap stop SIGINT
+trap stop SIGKILL
+
+run
