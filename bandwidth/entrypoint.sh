0e3c7a5984648b2a051daaf13aba9f30f580ff2c
Mark Abrams <mabrams@helpguest.com>
2020-09-01 11:32:33 -0400

    fix term defining count of bits

--- bandwidth/entrypoint.sh
+++ bandwidth/entrypoint.sh
@@ -4,9 +4,9 @@ LOW_LIMIT=$1
 rate=`echo "$(speedtest --no-upload --csv --csv-delimiter '|' | cut -d '|' -f 7)/1000000" | bc`
 
 if [[ "$rate" -gt "$LOW_LIMIT" ]]; then
-	echo "Rate $rate is acceptable."
+	echo "Rate $rate MB is acceptable."
 	exit 0;
 else
-	echo "Rate $rate Mb is too low. Threshold is $LOW_LIMIT"
+	echo "Rate $rate MB is too low. Threshold is $LOW_LIMIT"
 	exit "$rate"
 fi
