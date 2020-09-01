3c2b6c19bb0551cc82adcac4379dd5cd55eae117
Mark Abrams <mabrams@helpguest.com>
2020-09-01 13:04:31 -0400

    update rate reporting value to include 2 decimal places

--- bandwidth/entrypoint.sh
+++ bandwidth/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOW_LIMIT=$1
-rate=`echo "$(count=2; speedtest --no-upload --csv --csv-delimiter '|' | cut -d '|' -f 7)/8388608" | bc -l`
+rate=`echo "scale=2; $(speedtest --no-upload --csv --csv-delimiter '|' | cut -d '|' -f 7)/8388608" | bc -l`
 
 if [[ "$rate" -gt "$LOW_LIMIT" ]]; then
 	echo "Rate $rate MB/s is acceptable."
