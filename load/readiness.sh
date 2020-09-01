25723461ae0662915ba32c76f22a4e44fbd1289c
Mark Abrams <mabrams@helpguest.com>
2020-09-01 15:04:48 -0400

    add a readiness probe
    will probably want to remove the init container - it's redundant
    readiness.sh does exactly what the bandwidth entrypoint.sh does
    updated the speed-limit.yaml config
    updated the Dockerfile to include readiness.sh

---
+++ load/readiness.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LOW_LIMIT=$1
+rate=`echo "scale=2; $(speedtest --no-upload --csv --csv-delimiter '|' | cut -d '|' -f 7)/8388608" | bc -l`
+
+if (( $(echo "$rate > $LOW_LIMIT" | bc -l) )); then
+	echo "Rate $rate MB/s is acceptable."
+	exit 0;
+else
+	echo "Rate $rate MB/s is too low. Threshold is $LOW_LIMIT MB/s"
+	exit `echo "$rate/1" | bc`
+fi
