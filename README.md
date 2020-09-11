c1de1ce4f599fa856c8d90831f44eeadda9135ab
Mark Abrams <mabrams@helpguest.com>
2020-09-11 15:13:22 -0400

    setup a mechanism for start / stop of load

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 demo on using k3s to manage network fault tolerance
 
 1. Setup a k3s cluster
+	`curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig /home/pi/.kube/config --write-kubeconfig-mode 644 --tls-san 4bplus001" sh -`
 1. deploy the speed-limit.yaml
 1. watch the pods in the fault-tolerance namespace
-    `watch kubectl get pods -n fault-tolerance`
+	`watch kubectl get pods -n fault-tolerance`
 1. Throttle the speed allowance in the router (I am using a GL.inet 750s)
 
 Notice that the pod in the daemonset stops and exits. 
--- load/entrypoint.sh
+++ load/entrypoint.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 STARTED=1
+LOW="low"
+OK="ok"
 
 function stop() {
-	echo "Stopped generating traffic"
+	echo "Stop generating traffic"
 	STARTED=0;
 }
 
-function run() {
+function start() {
 	echo "Start creating download traffic"
 	while [[ $STARTED -gt 0 ]]; do
 		sleep 1;
 		curl -sfL --insecure https://cncf.io > /dev/null
 	done
+}
+
+function run() {
+	echo "run() - process to load"
+	bandwidth=$(cat /etc/ft-load-status)
+	if [ "$bandwidth" == "$LOW" ]; then
+		stop
+	if [ "$bandwidth" == "$OK" ]; then
+		start
+	fi
 }
 
 trap stop SIGTERM
